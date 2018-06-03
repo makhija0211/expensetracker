6e2493f32f9e8ece55cd357e3960eef53ddf2e6f
makhija0211 <mahimamakhija0211@gmail.com>
2018-06-03 22:43:10 +0530

    Last commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,24 +6,7 @@ import {FormBuilder , FormGroup , Validators} from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  rForm:FormGroup;
-  post:any;
-  name:string= '';
-  password:Text;
-  email:Text;
- 
-  constructor(private fb:FormBuilder){
-    this.rForm = fb.group({
-      'name':[null,Validators.required],
-      'email':[null,Validators.required],
-      'validate': ''
-    });
-  }
+  
 
-addPost(post){
-  this.name= post.name;
-  this.email=post.email;
-  this.password=post.password;
 
-}
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,13 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
-  apiKey: "AIzaSyA24rX1HPJvr-e9gnaCG4QI3jNiy-NrTLo",
-  authDomain: "loginform-4d7c7.firebaseapp.com",
-  databaseURL: "https://loginform-4d7c7.firebaseio.com",
-  projectId: "loginform-4d7c7",
-  storageBucket: "",
-  messagingSenderId: "920699317706"
+  production: false,
+ 
 
 };
 
