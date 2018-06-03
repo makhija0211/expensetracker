5e07f3d29b071ddb607f9bca5ada845250576d5e
makhija0211 <mahimamakhija0211@gmail.com>
2018-06-03 18:56:26 +0530

    Added login component , added bootstrap to index.html

---
+++ src/app/loginmodal/loginmodal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-loginmodal',
+  templateUrl: './loginmodal.component.html',
+  styleUrls: ['./loginmodal.component.css']
+})
+export class LoginmodalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
